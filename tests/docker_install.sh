47d4437237bba2d8fb85ab2ffe84deb50fb8d057
Connor Shea <connor.james.shea@gmail.com>
2019-04-24 11:44:50 -0600

    Get intl to work

--- tests/docker_install.sh
+++ tests/docker_install.sh
@@ -7,9 +7,10 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install apt-utils git libzip-dev zip libpng-dev -y
+apt-get install apt-utils git libzip-dev zip libpng-dev libicu-dev g++ -y
 
 # Install mysql driver
 # Here you can install any other extension that you need
+docker-php-ext-configure intl
 docker-php-ext-configure zip --with-libzip
 docker-php-ext-install pdo_mysql zip gd intl
