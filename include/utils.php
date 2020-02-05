0bb73e9997c714a994562a2af3707e71aa4e2c9c
Connor Shea <connor.james.shea@gmail.com>
2020-02-05 16:34:28 -0700

    Add buildSuiteUrl() function to utils.php.

--- include/utils.php
+++ include/utils.php
@@ -5726,3 +5726,47 @@ function getAppString($key)
 
     return $app_strings[$key];
 }
+
+/**
+ * Builds a URL for use with CRM, to make it easier to create consistent URLs.
+ * One of the parameters must be provided or the function with throw an
+ * exception. `null` values are ignored.
+ *
+ * @param string|null $module
+ * @param string|null $action
+ * @param string|null $return_module
+ * @param string|null $return_action
+ * @return string The string returned will be a SuiteCRM URL in the format 'index.php?module=Foo&action=bar', depending on what is provided to the function.
+ */
+function buildSuiteUrl($module = null, $action = null, $return_module = null, $return_action = null) {
+    $urlParts = [];
+
+    if (
+        is_null($module)
+        && is_null($action)
+        && is_null($return_module)
+        && is_null($return_action)
+    ) {
+        throw new Exception('Attempted to build an invalid SuiteCRM URL with no parameters.');
+    }
+
+    if (!is_null($module)) {
+        $urlParts[] = "module={$module}";
+    }
+
+    if (!is_null($action)) {
+        $urlParts[] = "action={$action}";
+    }
+
+    if (!is_null($return_module)) {
+        $urlParts[] = "return_module={$return_module}";
+    }
+
+    if (!is_null($return_action)) {
+        $urlParts[] = "return_action={$return_action}";
+    }
+
+    $joinedUrlParts = implode("&", $urlParts);
+
+    return "index.php?{$joinedUrlParts}";
+}
